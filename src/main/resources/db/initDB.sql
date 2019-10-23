8f1bfceaab528d34a73fedee33cca7099fdfd484
a.bondarenko <kballarock666@gmail.com>
2019-10-23 15:15:21 +0300

    - add SQL script to create and populate meals table

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,20 +1,19 @@
-DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
-#DROP SEQUENCE IF EXISTS global_seq;
-
-#CREATE SEQUENCE global_seq START WITH 100000;
+DROP TABLE IF EXISTS user_roles, users, meals;
 
 CREATE TABLE users
 (
-  id               INT AUTO_INCREMENT PRIMARY KEY,
+  id               INTEGER AUTO_INCREMENT PRIMARY KEY,
   name             VARCHAR(255)            NOT NULL,
   email            VARCHAR(255)            NOT NULL,
   password         VARCHAR(255)            NOT NULL,
   registered       TIMESTAMP DEFAULT now() NOT NULL,
-  enabled          BOOL DEFAULT TRUE       NOT NULL,
-  calories_per_day INTEGER DEFAULT 2000    NOT NULL
+  enabled          BOOL      DEFAULT TRUE  NOT NULL,
+  calories_per_day INTEGER   DEFAULT 2000  NOT NULL
 )
-AUTO_INCREMENT = 100000;
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  AUTO_INCREMENT = 100000;
+
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
 CREATE TABLE user_roles
@@ -23,4 +22,20 @@ CREATE TABLE user_roles
   role    VARCHAR(255),
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE meals
+(
+  id          INTEGER AUTO_INCREMENT PRIMARY KEY,
+  dateTime    DATETIME     NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  calories    INTEGER      NOT NULL,
+  user_id     INTEGER      NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+CREATE UNIQUE INDEX meals_unique_user_datetime_idx
+  ON meals (user_id, datetime);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,7 @@
-DELETE FROM user_roles;
-DELETE FROM users;
-#ALTER SEQUENCE global_seq RESTART WITH 100000;
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE users;
+TRUNCATE TABLE user_roles;
+TRUNCATE TABLE meals;
 
 INSERT INTO users (name, email, password) VALUES
 ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +10,18 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
 ('ROLE_USER', 100000),
 ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (id, dateTime, description, calories, user_id) VALUES
+#Meal for admin
+(1, '2015-07-01 14:00:00', 'Админ ланч', 510, 100001),
+(2, '2015-07-01 21:00:00', 'Админ ужин', 1500, 100001),
+
+#Meal for admin
+(3, '2015-05-30 10:00:00', 'Завтрак', 500, 100000),
+(4, '2015-05-30 13:00:00', 'Обед', 1000, 100000),
+(5, '2015-05-30 20:00:00', 'Ужин', 500, 100000),
+(6, '2015-05-31 10:00:00', 'Завтрак', 1000, 100000),
+(7, '2015-05-31 13:00:00', 'Обед', 500, 100000),
+(8, '2015-05-31 20:00:00', 'Ужин', 510, 100000);
+
+SET FOREIGN_KEY_CHECKS = 1;
