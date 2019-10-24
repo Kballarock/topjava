1e95695303b31f8a5afc4ee57dda78ae2faf49e2
a.bondarenko <kballarock666@gmail.com>
2019-10-24 17:29:25 +0300

    - fix SQL scripts;

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,9 +1,9 @@
-SET FOREIGN_KEY_CHECKS = 0;
-TRUNCATE TABLE user_roles;
-TRUNCATE TABLE meals;
-TRUNCATE TABLE users;
+DELETE FROM user_roles WHERE user_id;
+DELETE FROM meals WHERE id;
+DELETE FROM users WHERE id;
+
 ALTER TABLE users AUTO_INCREMENT = 100000;
-SET FOREIGN_KEY_CHECKS = 1;
+ALTER TABLE meals AUTO_INCREMENT = 100000;
 
 INSERT INTO users (name, email, password) VALUES
 ('User', 'user@yandex.ru', 'password'),
@@ -14,11 +14,8 @@ INSERT INTO user_roles (role, user_id) VALUES
 ('ROLE_ADMIN', 100001);
 
 INSERT INTO meals (date_time, description, calories, user_id) VALUES
-#Meal for admin
 ('2015-07-01 14:00:00', 'Админ ланч', 510, 100001),
 ('2015-07-01 21:00:00', 'Админ ужин', 1500, 100001),
-
-#Meal for admin
 ('2015-05-30 10:00:00', 'Завтрак', 500, 100000),
 ('2015-05-30 13:00:00', 'Обед', 1000, 100000),
 ('2015-05-30 20:00:00', 'Ужин', 500, 100000),
