e00a1073aa7999ece4b8f9e1638710186d744c6d
a.bondarenko <kballarock666@gmail.com>
2019-10-31 11:07:02 +0300

    - migrate to MySQL database

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,37 +1,42 @@
-DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS meals;
-DROP TABLE IF EXISTS users;
-DROP SEQUENCE IF EXISTS global_seq;
-
-CREATE SEQUENCE global_seq START WITH 100000;
+DROP TABLE IF EXISTS user_roles, users, meals;
 
 CREATE TABLE users
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  name             VARCHAR                 NOT NULL,
-  email            VARCHAR                 NOT NULL,
-  password         VARCHAR                 NOT NULL,
+  id               INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name             VARCHAR(255)            NOT NULL,
+  email            VARCHAR(255)            NOT NULL,
+  password         VARCHAR(255)            NOT NULL,
   registered       TIMESTAMP DEFAULT now() NOT NULL,
-  enabled          BOOL DEFAULT TRUE       NOT NULL,
-  calories_per_day INTEGER DEFAULT 2000    NOT NULL
-);
+  enabled          BOOL      DEFAULT TRUE  NOT NULL,
+  calories_per_day INTEGER   DEFAULT 2000  NOT NULL
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  AUTO_INCREMENT = 100000;
+
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
 CREATE TABLE user_roles
 (
   user_id INTEGER NOT NULL,
-  role    VARCHAR,
+  role    VARCHAR(255),
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE meals (
-  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  user_id     INTEGER   NOT NULL,
-  date_time   TIMESTAMP NOT NULL,
-  description TEXT      NOT NULL,
-  calories    INT       NOT NULL,
+CREATE TABLE meals
+(
+  id          INTEGER AUTO_INCREMENT PRIMARY KEY,
+  date_time   DATETIME                NOT NULL,
+  description VARCHAR(255)            NOT NULL,
+  calories    INTEGER                 NOT NULL,
+  user_id     INTEGER                 NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
 CREATE UNIQUE INDEX meals_unique_user_datetime_idx
-  ON meals (user_id, date_time);
+ON meals (user_id, date_time);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,7 +1,9 @@
-DELETE FROM user_roles;
-DELETE FROM meals;
-DELETE FROM users;
-ALTER SEQUENCE global_seq RESTART WITH 100000;
+DELETE FROM user_roles WHERE user_id;
+DELETE FROM meals WHERE id;
+DELETE FROM users WHERE id;
+
+ALTER TABLE users AUTO_INCREMENT = 100000;
+ALTER TABLE meals AUTO_INCREMENT = 100000;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
@@ -11,12 +13,12 @@ INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
 
-INSERT INTO meals (date_time, description, calories, user_id)
-VALUES ('2015-05-30 10:00:00', 'Завтрак', 500, 100000),
-       ('2015-05-30 13:00:00', 'Обед', 1000, 100000),
-       ('2015-05-30 20:00:00', 'Ужин', 500, 100000),
-       ('2015-05-31 10:00:00', 'Завтрак', 500, 100000),
-       ('2015-05-31 13:00:00', 'Обед', 1000, 100000),
-       ('2015-05-31 20:00:00', 'Ужин', 510, 100000),
-       ('2015-06-01 14:00:00', 'Админ ланч', 510, 100001),
-       ('2015-06-01 21:00:00', 'Админ ужин', 1500, 100001);
+INSERT INTO meals (date_time, description, calories, user_id) VALUES
+('2015-05-30 10:00:00', 'Завтрак', 500, 100000),
+('2015-05-30 13:00:00', 'Обед', 1000, 100000),
+('2015-05-30 20:00:00', 'Ужин', 500, 100000),
+('2015-05-31 10:00:00', 'Завтрак', 1000, 100000),
+('2015-05-31 13:00:00', 'Обед', 500, 100000),
+('2015-05-31 20:00:00', 'Ужин', 510, 100000),
+('2015-07-01 14:00:00', 'Админ ланч', 510, 100001),
+('2015-07-01 21:00:00', 'Админ ужин', 1500, 100001);
