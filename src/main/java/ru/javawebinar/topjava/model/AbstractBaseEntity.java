81ae87faee704cd5e11e723c4c26039e962aa1d4
a.bondarenko <kballarock666@gmail.com>
2019-11-13 15:53:18 +0300

    - migrate from postgreSQL to MySQL

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -11,9 +11,9 @@ import javax.persistence.*;
 public abstract class AbstractBaseEntity implements Persistable<Integer> {
     public static final int START_SEQ = 100000;
 
-    @Id
-    @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
+    @Id()
+    /*  @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)*/
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
 //  See https://hibernate.atlassian.net/browse/HHH-3718 and https://hibernate.atlassian.net/browse/HHH-12034
 //  Proxy initialization when accessing its identifier managed now by JPA_PROXY_COMPLIANCE setting
